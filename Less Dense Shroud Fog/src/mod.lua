db9a076e75e916d1dd650789644f3e6c02a8106e
ewigl <ewiglichtl@gmail.com>
2025-11-26 11:30:50 +0800

    LDSF: added extintion

--- Less Dense Shroud Fog/src/mod.lua
+++ Less Dense Shroud Fog/src/mod.lua
@@ -1,10 +1,13 @@
 -- [Mod 配置]
 -- [Mod Configuration]
 
+-- 雾的消光系数，游戏默认是 0.3499999940395355。数值越高光线越难以穿透（越看不清）。
+-- Fog extinction coefficient, Game's default is 0.3499999940395355. Higher values make light harder to penetrate (less visibility).
+local EXTINCTION = 0.35
+
 -- 默认是 1.0，数值越低雾越稀薄。不推荐设置过低，可能会影响游戏体验。
 -- Game's Default is 1.0; lower values result in thinner fog. Not recommended to set too low, as it may affect gameplay experience.
 local DENSITY_SCALE = 0.5
-
 
 -- 屏障雾厚度，默认是 8。设置为 0 可以移除屏障雾。
 -- Barrier fog thickness, default is 8. Setting to 0 removes barrier fog.
@@ -32,13 +35,11 @@ if data.barrierFogThickness then
 end
 
 if data.materials then
-    for i, material in ipairs(data.materials) do
-        if i ~= 1 then
-            material.densityScale = DENSITY_SCALE
-            print(string.format("[Less Dense Shroud Fog] Set densityScale of volumetric fog material %d to %.2f", i,
-                DENSITY_SCALE))
-        end
-    end
+    data.materials[2].extinction = EXTINCTION
+    print(string.format("[Less Dense Shroud Fog] Set extinction to %.1f", EXTINCTION))
+
+    data.materials[2].densityScale = DENSITY_SCALE
+    print(string.format("[Less Dense Shroud Fog] Set densityScale to %.1f", DENSITY_SCALE))
 else
     print("[Less Dense Shroud Fog] Error: VolumetricFog3Resource materials not found!")
 end
